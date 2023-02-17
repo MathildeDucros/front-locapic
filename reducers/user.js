d358d115ebb277a2a6eeceb8e38612d22254c867
Mathilde <ducros.mathilde@gmail.com>
2023-02-17 16:51:47 +0100

    first commit

---
+++ reducers/user.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: { nickname: null, places: [] },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateNickname: (state, action) => {
+      state.value.nickname = action.payload;
+    },
+    addPlace: (state, action) => {
+      state.value.places.push(action.payload);
+    },
+    removePlace: (state, action) => {
+      state.value.places = state.value.places.filter(
+        (e) => e.name !== action.payload
+      );
+    },
+    addAllPlace: (state, action) => {
+      state.value.places = action.payload;
+    },
+  },
+});
+
+export const { addAllPlace, updateNickname, addPlace, removePlace } =
+  userSlice.actions;
+export default userSlice.reducer;
